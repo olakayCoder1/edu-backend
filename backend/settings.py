05080f96c8428c6c938d9289aecf2411ebd3f87d
olakayCoder1 <programmerolakay@gmail.com>
2025-05-16 02:44:55 +0100

    "Updated 'NAME' in DATABASES configuration from 'prod_db.sqlite3' to 'db.sqlite3' in settings.py"

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'prod_db.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
