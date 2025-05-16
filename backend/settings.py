fa79d63cd4bb26392267454e8dc2ff18a85ae188
olakayCoder1 <programmerolakay@gmail.com>
2025-05-16 01:45:37 +0100

    `Added gunicorn to requirements.txt`

--- backend/settings.py
+++ backend/settings.py
@@ -70,14 +70,22 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'prod_db.sqlite3',
+        }
     }
-}
 
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'prod_db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
