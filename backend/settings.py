4de9b40dc41af23b7132413c7663cd9a7218ae0a
olakayCoder1 <programmerolakay@gmail.com>
2025-05-17 06:34:55 +0100

    `Update settings.py and urls.py to configure static files and media files`

--- backend/settings.py
+++ backend/settings.py
@@ -126,9 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Location to store static files (in development mode, static files will be stored in this folder)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# List of directories to look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
