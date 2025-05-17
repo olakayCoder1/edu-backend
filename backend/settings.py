442c5366de2df5bf4f2739a91e58b5517081003a
olakayCoder1 <programmerolakay@gmail.com>
2025-05-17 14:59:50 +0100

    `Update backend/settings.py to match existing prod_db.sqlite3 file`

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',  
         }
     }
 
